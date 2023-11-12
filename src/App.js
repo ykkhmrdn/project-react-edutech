85d9740f374ecc5281d90e09630703ecd196ad36
ykkhmrdn <ykkhmrdn@gmail.com>
2023-11-13 02:20:06 +0700

    Add Tailwind CSS and Next.js UI library, create AcmeLogo component, and update App.js and Home.jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'; // Pastikan path ini sesuai dengan lokasi file Home.jsx yang baru
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Tambahkan Route lainnya di sini */}
+      </Routes>
+    </Router>
   );
 }
 
