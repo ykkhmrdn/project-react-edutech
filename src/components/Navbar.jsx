c1211cace2e5039b0af5b4f41251ca482d18047d
ykkhmrdn <ykkhmrdn@gmail.com>
2023-11-14 00:47:54 +0700

    Update React dependencies and navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import LoginIcon from '@mui/icons-material/Login';
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -24,15 +25,16 @@ function Navbar() {
           <div class="flex items-center lg:order-2">
             <a
               href="/"
-              class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+              class=" dark:text-white hover:bg-primary-700 focus:ring-4 focus:ring-gray-300 rounded-lg px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800 text-center text-white text-base font-medium font-['Public Sans'] leading-normal"
             >
-              Log in
+            <LoginIcon />
+              Login
             </a>
             <a
               href="/"
-              class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+              class=" bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 lg:px-5 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 w-[167px] h-12 px-6 py-3 bg-gradient-to-b from-orange-300 to-orange-400 rounded-[100px] gap-2.5 inline-flex text-center text-white text-base font-normal font-['Public Sans'] leading-normal"
             >
-              Get started
+              Sign up for Free
             </a>
             <button
               data-collapse-toggle="mobile-menu-2"
@@ -88,7 +90,7 @@ function Navbar() {
               <li>
                 <a
                   href="/"
-                  class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-orange-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-primary-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-orange-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Company
                 </a>
@@ -96,7 +98,7 @@ function Navbar() {
               <li>
                 <a
                   href="/"
-                  class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-orange-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-primary-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-orange-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Marketplace
                 </a>
@@ -104,7 +106,7 @@ function Navbar() {
               <li>
                 <a
                   href="/"
-                  class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-orange-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-primary-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-orange-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Features
                 </a>
@@ -112,7 +114,7 @@ function Navbar() {
               <li>
                 <a
                   href="/"
-                  class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-orange-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  class="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-primary-700 lg:hover:bg-transparent lg:border-0 lg:hover:text-orange-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Team
                 </a>
